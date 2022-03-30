fcb91cc96d3333da69bc2ff372f32d9ab22e5acf
Pedro Henrique Wegner <pedrowegner@gmail.com>
2022-03-29 22:50:37 -0300

    alterado views de professor e aluno

--- departamento/view_aluno.py
+++ departamento/view_aluno.py
@@ -8,54 +8,54 @@ widget = QtWidgets.QStackedWidget()
 
 
 def executa_menu_aluno(usuario_logado=None):
-    tela_aluno = TelaAluno(usuario_logado=usuario_logado)
-    #
-    widget.addWidget(tela_aluno)
-    widget.setFixedHeight(864)
-    widget.setFixedWidth(1536)
-    widget.show()
-    app.exec_()
+    try:
+        tela_aluno = TelaAluno(usuario_logado=usuario_logado)
+        #
+        widget.addWidget(tela_aluno)
+        widget.setFixedHeight(864)
+        widget.setFixedWidth(1536)
+        widget.show()
+        app.exec_()
+    except Exception as e:
+        print(e)
 
 
 class TelaAluno(QDialog, DadosPessoa):
     def __init__(self, usuario_logado=None):
-        super(TelaAluno, self).__init__(usuario_logado=usuario_logado)
-        uic.loadUi(
-            r'C:\Users\pedro\Desktop\Trabalho Final Senai\projeto_final_desktop\departamento\gui\tela_menu_aluno.ui',
-            self
-        )
-        self.id_aluno()
-        self.btn = None
+        try:
+            super(TelaAluno, self).__init__(usuario_logado=usuario_logado)
+            uic.loadUi(
+                r'C:\Users\pedro\Desktop\Trabalho Final Senai\projeto_final_desktop\departamento\gui\tela_menu_aluno.ui',
+                self
+            )
+            self.id_aluno()
+            self.btn = None
 
-        # btn singals  - MENU - #
-        # HOME
-        self.home_construtor()
-        self.menu_btn_home.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_home))
-        self.menu_btn_home.clicked.connect(self.home_construtor)
+            # btn singals  - MENU - #
+            # HOME
+            self.home_construtor()
+            self.menu_btn_home.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_home))
+            self.menu_btn_home.clicked.connect(self.home_construtor)
 
-        # DISCIPLINAS
-        self.menu_btn_disciplinas.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_disciplinas))
-        self.menu_btn_disciplinas.clicked.connect(self.disciplinas_construtor)
+            # MODULOS
+            self.menu_btn_modulos.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_modulos))
+            self.menu_btn_modulos.clicked.connect(self.modulos_construtor)
 
-        # ATUALIZAR DADOS
-        self.menu_btn_att_dados.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_dados))
-        self.menu_btn_att_dados.clicked.connect(self.att_dados_construtor)
-        self.menu_btn_att_dados.clicked.connect(self.limpa_att_dados)
-        self.att_dados_btn_att_dados.clicked.connect(self.att_dados)
+            # ATUALIZAR DADOS
+            self.menu_btn_att_dados.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_dados))
+            self.menu_btn_att_dados.clicked.connect(self.att_dados_construtor)
+            self.menu_btn_att_dados.clicked.connect(self.limpa_att_dados)
+            self.att_dados_btn_att_dados.clicked.connect(self.att_dados)
 
-        # ATUALIZAR SENHA
-        self.menu_btn_att_senha.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_senha))
-        self.menu_btn_att_senha.clicked.connect(self.limpa_att_senha)
-        self.att_senha_btn_visualizar_senha.clicked.connect(self.visualizar_senha)
-        self.att_senha_btn_att_senha.clicked.connect(self.atualizar_senha)
-        self.att_senha_btn_att_senha.clicked.connect(self.limpa_att_senha)
+            # ATUALIZAR SENHA
+            self.menu_btn_att_senha.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_senha))
+            self.menu_btn_att_senha.clicked.connect(self.limpa_att_senha)
+            self.att_senha_btn_visualizar_senha.clicked.connect(self.visualizar_senha)
+            self.att_senha_btn_att_senha.clicked.connect(self.atualizar_senha)
+            self.att_senha_btn_att_senha.clicked.connect(self.limpa_att_senha)
 
-        # SOLICITAR DISCIPLINA
-        self.menu_btn_solicitar_disciplina.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_solicitar_disciplina))
-        self.menu_btn_solicitar_disciplina.clicked.connect(self.solicitar_disciplina_construtor)
-        self.solicitar_disciplina_btn_solicitar_disciplina.clicked.connect(self.solicitar_disciplina)
-        self.solicitar_disciplina_btn_solicitar_disciplina.clicked.connect(self.solicitar_disciplina_construtor)
-
+        except Exception as e:
+            print(e)
         # FIM btn singals  - MENU - #
 
     # DEFINE ID ALUNO LOGADO #
@@ -80,66 +80,47 @@ class TelaAluno(QDialog, DadosPessoa):
             f"Bem-vindo {self.usuario_logado['nome_pessoa']} {self.usuario_logado['sobrenome_pessoa']}"
         )
 
-    def disciplinas_construtor(self):
-        # coluna 1
-        for i in reversed(range(self.disciplinas_layout_disciplina_1.count())):
-            displina_removida = self.disciplinas_layout_disciplina_1.itemAt(i).widget()
-            self.disciplinas_layout_disciplina_1.removeWidget(displina_removida)
-            displina_removida.setParent(None)
-        # coluna 2
-        for i in reversed(range(self.disciplinas_layout_disciplina_2.count())):
-            displina_removida = self.disciplinas_layout_disciplina_2.itemAt(i).widget()
-            self.disciplinas_layout_disciplina_2.removeWidget(displina_removida)
-            displina_removida.setParent(None)
-        #
-        comando_sql = f"SELECT DeDi.id, DeDi.disciplina " \
-                      f"FROM departamento_aluno DeAl " \
-                      f"INNER JOIN departamento_aluno_disciplina DeAlDi " \
-                      f"ON DeAlDi.aluno_id = DeAl.id " \
-                      f"INNER JOIN departamento_disciplina DeDi " \
-                      f"ON DeDi.id = DeAlDi.disciplina_id " \
-                      f"WHERE DeAl.id={self.usuario_logado['id_aluno']}"
-        disciplinas_selecionadas = self.conexao.select_all(comando_sql=comando_sql)
+    def modulos_construtor(self):
+        try:
+            # coluna 1
+            for i in reversed(range(self.modulos_layout_modulo_1.count())):
+                modulo_removido = self.modulos_layout_modulo_1.itemAt(i).widget()
+                self.modulos_layout_modulo_1.removeWidget(modulo_removido)
+                modulo_removido.setParent(None)
+            # coluna 2
+            for i in reversed(range(self.modulos_layout_modulo_2.count())):
+                modulo_removido = self.modulos_layout_modulo_2.itemAt(i).widget()
+                self.modulos_layout_modulo_2.removeWidget(modulo_removido)
+                modulo_removido.setParent(None)
 
-        for numero, disciplina in enumerate(disciplinas_selecionadas):
-                self.btn = QtWidgets.QPushButton(disciplina[1])
-                dict_discplina = {
-                    'id_disciplina': disciplina[0],
-                    'disciplina': disciplina[1],
+            comando_sql = f"SELECT DeMo.id, DeMo.modulo " \
+                          f"FROM departamento_aluno DeAl " \
+                          f"INNER JOIN departamento_aluno_modulo DeAlMo " \
+                          f"ON DeAlMo.aluno_id = DeAl.id " \
+                          f"INNER JOIN departamento_modulo DeMo " \
+                          f"ON DeAlMo.modulo_id = DeMo.id " \
+                          f"WHERE DeAl.id={self.usuario_logado['id_aluno']};"
+            modulos_selecionados = self.conexao.select_all(comando_sql=comando_sql)
+            for x, modulo in enumerate(modulos_selecionados):
+                self.btn = QtWidgets.QPushButton(modulo[1])
+                dict_modulo = {
+                    'id_modulo': modulo[0],
+                    'modulo': modulo[1],
                 }
-                self.btn.clicked.connect(lambda ch, dict=dict_discplina: self.disciplina_construtor(dict_disciplina=dict))
-                if numero % 2 == 0:
-                    self.disciplinas_layout_disciplina_1.addWidget(self.btn)
+                self.btn.clicked.connect(lambda ch, dict=dict_modulo: self.modulo_construtor(dict_modulo=dict))
+                if x % 2 == 0:
+                    self.modulos_layout_modulo_1.addWidget(self.btn)
                 else:
-                    self.disciplinas_layout_disciplina_2.addWidget(self.btn)
+                    self.modulos_layout_modulo_2.addWidget(self.btn)
+        except Exception as e:
+            print(e)
 
-    def disciplina_construtor(self, dict_disciplina=None):
-        self.menu_stacked.setCurrentWidget(self.disciplinas_disciplina)
-        self.disciplina_label_disciplina.setText(f"{dict_disciplina['disciplina']}")
-
-    def solicitar_disciplina_construtor(self):
-        self.input_disciplinas(input=self.solicitar_disciplina_input_disciplinas)
-
+    def modulo_construtor(self, dict_modulo=None):
+        self.menu_stacked.setCurrentWidget(self.modulos_modulo)
+        self.modulo_label_modulo.setText(f"{dict_modulo['modulo']}")
     # FIM CONSTRUTORES DE TELA #
 
     # FUNCOES DE TELA #
-
-    # DISCIPLINAS #
-
-    # FIM DISCIPLINAS #
-
-    # SOLICITA DISCIPLINA #
-    def solicitar_disciplina(self):
-        disc_selecionadas = self.solicitar_disciplina_input_disciplinas.selectedItems()
-        disciplinas = []
-        for disciplina in disc_selecionadas:
-            disciplinas.append(disciplina.text())
-        disciplinas = tuple(disciplinas)
-
-        aluno = Aluno(disciplina=disciplinas)
-        aluno.id = self.usuario_logado['id_aluno']
-        aluno.matricula_disciplina()
-    # FIM SOLICITA DISCIPLINA #
 
     # FIM FUNCOES DE TELA #
 
--- departamento/view_professor.py
+++ departamento/view_professor.py
@@ -18,38 +18,41 @@ def executa_menu_professor(usuario_logado=None):
 
 class TelaProfessor(QDialog, DadosPessoa):
     def __init__(self, usuario_logado=None):
-        super(TelaProfessor, self).__init__(usuario_logado=usuario_logado)
-        uic.loadUi(
-            r'C:\Users\pedro\Desktop\Trabalho Final Senai\projeto_final_desktop\departamento\gui\tela_menu_professor.ui',
-            self
-        )
-        self.id_professor()
-        self.btn = None
+        try:
+            super(TelaProfessor, self).__init__(usuario_logado=usuario_logado)
+            uic.loadUi(
+                r'C:\Users\pedro\Desktop\Trabalho Final Senai\projeto_final_desktop\departamento\gui\tela_menu_professor.ui',
+                self
+            )
+            self.id_professor()
+            self.btn = None
 
-        # btn singals  - MENU - #
-        # HOME
-        self.home_construtor()
-        self.menu_btn_home.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_home))
-        self.menu_btn_home.clicked.connect(self.home_construtor)
+            # btn singals  - MENU - #
+            # HOME
+            self.home_construtor()
+            self.menu_btn_home.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_home))
+            self.menu_btn_home.clicked.connect(self.home_construtor)
 
-        # DISCIPLINAS
-        self.menu_btn_disciplinas.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_disciplinas))
-        self.menu_btn_disciplinas.clicked.connect(self.disciplinas_construtor)
+            # DISCIPLINAS
+            self.menu_btn_modulos.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_modulos))
+            self.menu_btn_modulos.clicked.connect(self.modulos_construtor)
 
-        # ATUALIZAR DADOS
-        self.menu_btn_att_dados.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_dados))
-        self.menu_btn_att_dados.clicked.connect(self.att_dados_construtor)
-        self.menu_btn_att_dados.clicked.connect(self.limpa_att_dados)
-        self.att_dados_btn_att_dados.clicked.connect(self.att_dados)
+            # ATUALIZAR DADOS
+            self.menu_btn_att_dados.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_dados))
+            self.menu_btn_att_dados.clicked.connect(self.att_dados_construtor)
+            self.menu_btn_att_dados.clicked.connect(self.limpa_att_dados)
+            self.att_dados_btn_att_dados.clicked.connect(self.att_dados)
 
-        # ATUALIZAR SENHA
-        self.menu_btn_att_senha.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_senha))
-        self.menu_btn_att_senha.clicked.connect(self.limpa_att_senha)
-        self.att_senha_btn_visualizar_senha.clicked.connect(self.visualizar_senha)
-        self.att_senha_btn_att_senha.clicked.connect(self.atualizar_senha)
-        self.att_senha_btn_att_senha.clicked.connect(self.limpa_att_senha)
+            # ATUALIZAR SENHA
+            self.menu_btn_att_senha.clicked.connect(lambda: self.menu_stacked.setCurrentWidget(self.menu_att_senha))
+            self.menu_btn_att_senha.clicked.connect(self.limpa_att_senha)
+            self.att_senha_btn_visualizar_senha.clicked.connect(self.visualizar_senha)
+            self.att_senha_btn_att_senha.clicked.connect(self.atualizar_senha)
+            self.att_senha_btn_att_senha.clicked.connect(self.limpa_att_senha)
 
-        # FIM btn singals  - MENU - #
+            # FIM btn singals  - MENU - #
+        except Exception as e:
+            print(e)
 
     # DEFINE ID PROFESSOR LOGADO #
     def id_professor(self):
@@ -72,52 +75,56 @@ class TelaProfessor(QDialog, DadosPessoa):
             f"Bem-vindo {self.usuario_logado['nome_pessoa']} {self.usuario_logado['sobrenome_pessoa']}"
         )
 
-    def disciplinas_construtor(self):
-        # coluna 1
-        for i in reversed(range(self.disciplinas_layout_disciplina_1.count())):
-            displina_removida = self.disciplinas_layout_disciplina_1.itemAt(i).widget()
-            self.disciplinas_layout_disciplina_1.removeWidget(displina_removida)
-            displina_removida.setParent(None)
-        # coluna 2
-        for i in reversed(range(self.disciplinas_layout_disciplina_2.count())):
-            displina_removida = self.disciplinas_layout_disciplina_2.itemAt(i).widget()
-            self.disciplinas_layout_disciplina_2.removeWidget(displina_removida)
-            displina_removida.setParent(None)
-        #
-        comando_sql = f"SELECT * " \
-                      f"FROM departamento_disciplina DeDi " \
-                      f"INNER JOIN departamento_professor DePr " \
-                      f"ON DeDi.professor_id = DePr.id " \
-                      f"WHERE DePr.id={self.usuario_logado['id_professor']}"
-        disciplinas_selecionadas = self.conexao.select_all(comando_sql=comando_sql)
-
-        for numero, disciplina in enumerate(disciplinas_selecionadas):
-            self.btn = QtWidgets.QPushButton(disciplina[1])
-            dict_discplina = {
-                'id_disciplina': disciplina[0],
-                'disciplina': disciplina[1],
-            }
-            self.btn.clicked.connect(lambda ch, dict=dict_discplina: self.disciplina_construtor(dict_disciplina=dict))
-            if numero % 2 == 0:
-                self.disciplinas_layout_disciplina_1.addWidget(self.btn)
-            else:
-                self.disciplinas_layout_disciplina_2.addWidget(self.btn)
-
-    def disciplina_construtor(self, dict_disciplina=None):
+    def modulos_construtor(self):
         try:
-            self.menu_stacked.setCurrentWidget(self.disciplinas_disciplina)
-            self.disciplina_label_disciplina.setText(f"{dict_disciplina['disciplina']}")
-            self.disciplina_output_alunos_matriculados.clear()
+            # coluna 1
+            for i in reversed(range(self.modulos_layout_modulo_1.count())):
+                modulo_removido = self.modulos_layout_modulo_1.itemAt(i).widget()
+                self.modulos_layout_modulo_1.removeWidget(modulo_removido)
+                modulo_removido.setParent(None)
+            # coluna 2
+            for i in reversed(range(self.modulos_layout_modulo_2.count())):
+                modulo_removido = self.modulos_layout_modulo_2.itemAt(i).widget()
+                self.modulos_layout_modulo_2.removeWidget(modulo_removido)
+                modulo_removido.setParent(None)
+            #
+            comando_sql = f"SELECT * " \
+                          f"FROM departamento_modulo DeMo " \
+                          f"INNER JOIN departamento_professor_modulo DePrMo " \
+                          f"ON DePrMo.modulo_id = DeMo.id " \
+                          f"INNER JOIN departamento_professor DePr " \
+                          f"ON DePrMo.professor_id = DePr.id " \
+                          f"WHERE DePr.id={self.usuario_logado['id_professor']}"
+            modulos_selecionados = self.conexao.select_all(comando_sql=comando_sql)
+            for x, modulo in enumerate(modulos_selecionados):
+                self.btn = QtWidgets.QPushButton(modulo[1])
+                dict_modulo = {
+                    'id_modulo': modulo[0],
+                    'modulo': modulo[1],
+                }
+                self.btn.clicked.connect(lambda ch, dict=dict_modulo: self.modulo_construtor(dict_modulo=dict))
+                if x % 2 == 0:
+                    self.modulos_layout_modulo_1.addWidget(self.btn)
+                else:
+                    self.modulos_layout_modulo_2.addWidget(self.btn)
+        except Exception as e:
+            print(e)
+
+    def modulo_construtor(self, dict_modulo=None):
+        try:
+            self.menu_stacked.setCurrentWidget(self.modulos_modulo)
+            self.modulo_label_modulo.setText(f"{dict_modulo['modulo']}")
+            self.modulo_output_alunos_matriculados.clear()
             comando_sql = f"SELECT PePe.nome, PePe.sobrenome " \
-                          f"FROM departamento_aluno_disciplina DeAlDi " \
+                          f"FROM departamento_aluno_modulo DeAlMo " \
                           f"INNER JOIN departamento_aluno DeAl " \
-                          f"ON DeAlDi.aluno_id = DeAl.id " \
+                          f"ON DeAlMo.aluno_id = DeAl.id " \
                           f"INNER JOIN pessoa_pessoa PePe " \
                           f"ON DeAl.pessoa_id = PePe.id " \
-                          f"WHERE DeAlDi.disciplina_id={dict_disciplina['id_disciplina']};"
+                          f"WHERE DeAlMo.modulo_id={dict_modulo['id_modulo']};"
             alunos_matriculados = self.conexao.select_all(comando_sql=comando_sql)
             for aluno in alunos_matriculados:
-                self.disciplina_output_alunos_matriculados.addItem(f"{aluno[0]} {aluno[1]}")
+                self.modulo_output_alunos_matriculados.addItem(f"{aluno[0]} {aluno[1]}")
         except Exception as e:
             print(e)
 
